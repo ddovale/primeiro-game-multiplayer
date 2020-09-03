c8ea4d7df865c22188bb4ca908f15bb612f7b2ae
ddovale <dovale.daniel07@gmail.com>
2020-09-03 02:14:20 +0000

    Gitpod improves

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log('> Script started')
+const express = require('express')
+const webApp = express()
+const webServer = require('http').createServer(webApp)
+
+webApp.get('/', function(req, res){
+  res.sendFile(__dirname + '/game/index.html')
+})
+
+webServer.listen(3000, function(){
+  console.log('> Server listening on port:',3000)
+});
